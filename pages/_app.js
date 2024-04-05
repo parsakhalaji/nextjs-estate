7cfcb88815e48625444708727c26d1b454e4c542
Parsa Khalaj <parsa.khalaji@gmail.com>
2024-04-05 19:52:48 +0430

    create webpage templates

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Footer from "@/components/templates/footer/Footer";
+import Header from "@/components/templates/header/Header";
 import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
+            <style jsx>{``}</style>
+        </>
+    );
 }
